02d9703c20da0ee1ecf904cae7e64e6e4be7f433
Niclas Forsman <niclas.forsman@futurice.com>
2019-05-06 10:47:04 +0300

    add boilerplate section 2

---
+++ materials/section-2/webapp/src/webapp/handler.clj
@@ -0,0 +1,35 @@
+(ns webapp.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.util.response :as r]
+            [ring.middleware.params :as request-middleware]
+            [ring.middleware.json :as json-middleware]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+;; step 1 - lein new compojure webapp
+;; step 2 - show how compojure works
+;; step 3 - create a fake db atom : Attention! Missing middleware!
+;; step 4 - remove unused middleware (defaults)
+;; step 5 - add ring.middleware.json
+;; step 6 - add ring.middleware.params
+
+(def todos (atom []))
+
+(def id-counter (atom 0))
+
+(defroutes app-routes
+  (GET "/todos" []
+    (r/response @todos))
+  (POST "/todos" [name]
+    (swap! todos conj {:id (swap! id-counter inc) :name name :done false})
+    (r/response @todos))
+  (DELETE "/todos/:id" [id]
+    (reset! todos (remove (fn [todo] (= (:id todo) (Integer/parseInt id))) @todos))
+    (r/response @todos))
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes
+      request-middleware/wrap-params
+      json-middleware/wrap-json-params
+      json-middleware/wrap-json-response))
